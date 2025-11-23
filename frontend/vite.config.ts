9faaf12b03c6ad0bc68109abb3d83900a07733b0
Upendo1 <upendochacha@gmail.com>
2025-11-23 15:56:42 +0300

    update changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "https://educonnect-mern-stack-final-project.onrender.com",
     port: 5173,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
