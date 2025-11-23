f52d58f5263426510be9ff069d862268f3ccf768
Upendo1 <upendochacha@gmail.com>
2025-11-23 19:10:35 +0300

    update changes

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -30,7 +30,7 @@ const Auth = () => {
     try {
       if (isLogin) {
         // LOGIN
-        const res = await api.post(${API_BASE}/auth/login, {
+        const res = await api.post(`${API_BASE}/auth/login`, {
           email,
           password,
         });
@@ -46,37 +46,11 @@ const Auth = () => {
           title: 'Welcome back!',
           description: 'You have successfully logged in.',
         });
-         if (user) {
-    const role = user.role?.toLowerCase();
-    // console.log("--"+user.role?.toLowerCase()+"--")
-    switch (role) {
-      case "student":
-        return <StudentDashboard />;
 
-      case "teacher":
-        return <TeacherDashboard />;
-
-      case "admin":
-        return <AdminDashboard />;
-
-
-      default:
-        return (
-        <div className="min-h-screen flex flex-col items-center justify-center gap-4">
-          {/* Spinner Loader */}
-          <div className="animate-spin rounded-full h-12 w-12 border-4 border-gray-300 border-t-primary"></div>
-
-        <p className="text-lg font-medium text-muted-foreground">
-  Still loading… this may take a moment. You can refresh the page if it takes too long.
-</p>
-        </div>
-      );
-    }
-  }
-        // navigate('/dashboard'); // ✅ NOW THIS WORKS
+        navigate('/'); // ✅ NOW THIS WORKS
       } else {
         // REGISTER
-        const res = await api.post(${API_BASE}/auth/register, {
+        const res = await api.post(`${API_BASE}/auth/register`, {
           fullName,
           email,
           password,
